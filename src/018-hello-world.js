b511f96e947c726f6c5ff4ad8f994b625b742ff5
George Mandis <george@mand.is>
2019-02-09 02:45:29 +0100

    Added binary unicode matrix solution

---
+++ src/018-hello-world.js
@@ -0,0 +1,40 @@
+/**
+ *  Hello World #<number>
+ */
+
+module.exports = () => {
+  const helloWorldMatrix = [
+    0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,
+    0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,1,
+    0,0,0,0,0,0,0,0,0,1,1,0,1,1,0,0,
+    0,0,0,0,0,0,0,0,0,1,1,0,1,1,0,0,
+    0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,
+    0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,0,
+    0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,1,0,1,0,1,1,1,
+    0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,
+    0,0,0,0,0,0,0,0,0,1,1,1,0,0,1,0,
+    0,0,0,0,0,0,0,0,0,1,1,0,1,1,0,0,
+    0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,0
+  ];
+
+  let returnValue = "";
+  for (let row = 0; row < helloWorldMatrix.length; row += 16) {
+    const binaryLetter = [];
+    for (let x = 0; x < 16; x++) {
+      binaryLetter.push(helloWorldMatrix[row + x]);
+    }
+
+    returnValue += String.fromCharCode(parseInt(binaryLetter.join(""), 2));
+  }
+
+  return returnValue;
+};
+
+/**
+ * If run directly "Hello, World" will be written to the console
+ * If run as a module during the tests it return "Hello, World" as
+ * a value to assert correctness;
+ */
+
+module.id == "." ? console.log(module.exports()) : null;
