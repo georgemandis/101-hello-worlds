2e66e7c6892cc13da8e17d90e5a242d09b66416f
George Mandis <george@mand.is>
2020-01-29 11:51:49 -0500

    Added version using Object.keys()

---
+++ src/022-hello-world.js
@@ -0,0 +1,21 @@
+/**
+ *  Hello World #22
+ */
+
+module.exports = () => {
+  const HelloWorldClass = {
+    Hello: {},
+    ", ": {},
+    World: {}
+  };
+
+  return (Object.keys(HelloWorldClass).join(""));
+};
+
+/**
+ * If run directly "Hello, World" will be written to the console
+ * If run as a module during the tests it return "Hello, World" as
+ * a value to assert correctness;
+ */
+
+module.id == "." ? console.log(module.exports()) : null;
