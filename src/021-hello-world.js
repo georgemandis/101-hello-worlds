d282d3831378771f713a73628c051e9f39a80aec
George Mandis <george@mand.is>
2020-01-29 11:35:14 -0500

    Curious combo of string concat + object

---
+++ src/021-hello-world.js
@@ -0,0 +1,15 @@
+/**
+ *  Hello World #21
+ */
+
+module.exports = () => {    
+  return ({valueOf: () => "Hello, World"} + "");
+};
+
+/**
+ * If run directly "Hello, World" will be written to the console
+ * If run as a module during the tests it return "Hello, World" as
+ * a value to assert correctness;
+ */
+
+module.id == "." ? console.log(module.exports()) : null;
