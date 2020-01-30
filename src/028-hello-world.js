8336b8277166e688a0332124ad10fbb5ee24f625
kirjavascript <snkenjoi@gmail.com>
2020-01-30 23:43:42 +0000

    #028

---
+++ src/028-hello-world.js
@@ -0,0 +1,15 @@
+/**
+ *  Hello World #1
+ */
+
+module.exports = () => {
+    return /Hello, World/.source;
+};
+
+/**
+ * If run directly "Hello, World" will be written to the console
+ * If run as a module during the tests it return "Hello, World" as
+ * a value to assert correctness;
+ */
+
+module.id == "." ? console.log(module.exports()) : null
