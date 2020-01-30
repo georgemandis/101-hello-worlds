18188513f6d134fe1fe208bda66313ec2a02c04e
kirjavascript <snkenjoi@gmail.com>
2020-01-30 22:07:49 +0000

    #026

---
+++ src/026-hello-world.js
@@ -0,0 +1,22 @@
+/**
+ *  Hello World #26
+ */
+
+module.exports = () => {
+  function _(s='') {
+    return new Proxy({}, {
+      get(_, key) {
+        return s ? () => [s, ' ' + key] + [] : _(s + key)
+      },
+    });
+  }
+  return _().Hello.World();
+};
+
+/**
+ * If run directly "Hello, World" will be written to the console
+ * If run as a module during the tests it return "Hello, World" as
+ * a value to assert correctness;
+ */
+
+module.id == "." ? console.log(module.exports()) : null
