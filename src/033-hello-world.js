d5dab0bebef88ea8cc4eda86f4253584428d1eeb
Fabian Tamp <fabian.tamp@gmail.com>
2020-01-30 17:10:44 -0500

    Add a new Hello World!
    I made this number 33 so we wouldn't have to renumber things when people merge 😅

---
+++ src/033-hello-world.js
@@ -0,0 +1,22 @@
+/**
+ *  Hello World #33
+ */
+
+module.exports = () => {
+  var yes = '🥈🥥🥬🍬👯🔬🤠🕗🍯🩲👬🥤';
+  var no = [];
+  for (var i = 1; i < yes.length; i+=2) {
+    no = no.concat([yes.charAt(i)]);
+  }
+  return no.map(yes => (yes.charCodeAt(0) & 0xFF))
+           .map(no => String.fromCharCode(no))
+           .join('');
+};
+
+  /**
+   * If run directly "Hello, World" will be written to the console
+   * If run as a module during the tests it return "Hello, World" as
+   * a value to assert correctness;
+   */
+
+  module.id == "." ? console.log(module.exports()) : null;
